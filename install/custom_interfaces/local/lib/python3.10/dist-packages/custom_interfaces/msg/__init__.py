c89f41cd146f54a39876f116fe511180663d9967
JungHyeon Choi <83526938+checkjunghyeon@users.noreply.github.com>
2025-07-08 14:49:55 +0900

    Delete install directory

--- install/custom_interfaces/local/lib/python3.10/dist-packages/custom_interfaces/msg/__init__.py
+++
@@ -1,3 +0,0 @@
-from custom_interfaces.msg._inference_result import InferenceResult  # noqa: F401
-from custom_interfaces.msg._send_img import SendImg  # noqa: F401
-from custom_interfaces.msg._yolov8_inference import Yolov8Inference  # noqa: F401
